a84b81f62a6d8b2e83a92b63efac770b693c5726
TestMonBlock <maidzmahi@gmail.com>
2025-09-20 17:04:15 +1200

    Remove old custom domain

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  root: './',
-  server: {
-    port: 5173
-  }
-})
+  base: "/baked-bread-miner/"  // 👈 IMPORTANT for GitHub Pages
+});
